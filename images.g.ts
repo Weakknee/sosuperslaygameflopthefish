94a3c5c3f5927dcf1c4f641330326e5588e32581
Weakknee <105798497+Weakknee@users.noreply.github.com>
2022-05-18 15:14:48 +0100

    Update pxt.json, main.blocks, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

---
+++ images.g.ts
@@ -0,0 +1,29 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . . f f f . . 
+. f f 2 2 2 f . 
+f 2 f 2 2 f f . 
+f 2 f 2 f d d f 
+f 2 f 2 2 f f . 
+f 2 f 2 2 2 f . 
+. f f 2 f 2 f . 
+. . . f . f . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
